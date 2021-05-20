8cf9a027568f4df48c064ea2d904fac1422df7e6
LioCroons <liomanjate@gmail.com>
2021-05-20 18:30:05 -0300

    /usuarios

--- src/main/java/com/uniamerica/unijobsbackend/controller/UsuarioController.java
+++ src/main/java/com/uniamerica/unijobsbackend/controller/UsuarioController.java
@@ -3,24 +3,62 @@ package com.uniamerica.unijobsbackend.controller;
 import com.uniamerica.unijobsbackend.model.Usuario;
 import com.uniamerica.unijobsbackend.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
-    @Autowired
+    @Autowired //injecao de dependecias conceito - SOLID eh um padrao de design de codigo
     private UsuarioRepository usuarioRepository;
 
     @GetMapping
-    public List<Usuario> listar() {
-        return usuarioRepository.findAll();
+    public ResponseEntity<List<Usuario>>BuscarTodos(){
+        return ResponseEntity.ok(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarUsuario(@PathVariable Integer id){
+        Optional<Usuario> user = usuarioRepository.findById(id);
+        if (user.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user.get());
     }
     @PostMapping
-    @ResponseStatus
-    public Usuario adicionar(@RequestBody Usuario usuario) {
-        return usuarioRepository.save(usuario);
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario user){
+        Usuario saved = usuarioRepository.save(user);
+        return new ResponseEntity<Usuario>(saved, HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Integer id, @RequestBody Usuario usuario){
+        Optional<Usuario> user = usuarioRepository.findById(id);
+        if (user.isEmpty()) {
+            return  ResponseEntity.notFound().build();
+        }
+        Usuario saved = user.get();
+        saved.setEmail(usuario.getEmail());
+        saved.setSenha(usuario.getSenha());
+        saved.setNome(usuario.getNome());
+        saved.setCelular(usuario.getCelular());
+        saved.setRa(usuario.getCelular());
+        saved = usuarioRepository.save(saved);
+        return ResponseEntity.ok(saved);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Usuario> removerUsuario(@PathVariable Integer id){
+        Optional<Usuario> user = usuarioRepository.findById(id);
+        if (user.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        usuarioRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
