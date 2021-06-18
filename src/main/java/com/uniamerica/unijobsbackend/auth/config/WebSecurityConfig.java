d8c69eb2dcd4f8a25a9e13f025f6ed0380937cdd
Nelson J S Baez <bluexbaez@gmail.com>
2021-06-17 21:30:55 -0300

    Adicionado cors Conf

--- src/main/java/com/uniamerica/unijobsbackend/auth/config/WebSecurityConfig.java
+++ src/main/java/com/uniamerica/unijobsbackend/auth/config/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
-        httpSecurity.csrf().disable()
+        httpSecurity.cors().and().csrf().disable()
                 // dont authenticate this particular request
                 .authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
                 // all other requests need to be authenticated
---
+++ src/main/java/com/uniamerica/unijobsbackend/configs/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.uniamerica.unijobsbackend.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+}
