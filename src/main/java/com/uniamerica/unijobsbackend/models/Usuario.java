a706ae6ea5feb9c4cf24e3e7f8406ea2cdc232c1
Nelson J S Baez <bluexbaez@gmail.com>
2021-06-30 09:48:03 -0300

    corrigido model de usuario

--- src/main/java/com/uniamerica/unijobsbackend/models/Usuario.java
+++ src/main/java/com/uniamerica/unijobsbackend/models/Usuario.java
@@ -17,7 +17,7 @@ public class Usuario implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @NotNull
     private String email;
