8062be8fe21246b650dc1cfd435619bae9aec2c6
Brayan <BrayanWilis@outlook.com>
2021-09-09 20:32:59 -0300

    primeira correcao dos testes de produto e tipoProduto

--- src/main/java/com/uniamerica/unijobsbackend/Testes/TesteProdutoRepository.java
+++ src/main/java/com/uniamerica/unijobsbackend/Testes/TesteProdutoRepository.java
@@ -9,12 +9,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @DataJpaTest
 public class TesteProdutoRepository {
 
-
-    RepositorioProduto repository = new RepositorioProduto();
+    @Autowired
+    private RepositorioProduto repository;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
