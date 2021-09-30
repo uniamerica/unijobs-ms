26ba7d2674f3b19f2ed33bd980be3db1099a9047
Willian <willianthiagofozz@hotmail.com>
2021-09-30 11:54:17 -0300

    Finalizados os testes de CRUD completos da classe TipoServicoController

--- src/test/java/com/uniamerica/unijobsbackend/controllers/TipoServicoControllerTest.java
+++ src/test/java/com/uniamerica/unijobsbackend/controllers/TipoServicoControllerTest.java
@@ -1,46 +1,130 @@
 package com.uniamerica.unijobsbackend.controllers;
 
-import com.uniamerica.unijobsbackend.auth.config.JwtAuthenticationEntryPoint;
-import com.uniamerica.unijobsbackend.auth.config.JwtTokenUtil;
-import com.uniamerica.unijobsbackend.auth.services.UserService;
+import com.uniamerica.unijobsbackend.Excessoes.RecursoNaoEncontradoExcessao;
+import com.uniamerica.unijobsbackend.controllers.TipoServicoController;
 import com.uniamerica.unijobsbackend.dto.ServicoDTO;
+import com.uniamerica.unijobsbackend.dto.TipoServicoDTO;
 import com.uniamerica.unijobsbackend.models.Servico;
 import com.uniamerica.unijobsbackend.models.TipoServico;
-import com.uniamerica.unijobsbackend.services.ServicoService;
+import com.uniamerica.unijobsbackend.repositories.ServicoRepository;
+import com.uniamerica.unijobsbackend.repositories.TipoServicoRepository;
+import com.uniamerica.unijobsbackend.repositories.UsuarioRepository;
 import com.uniamerica.unijobsbackend.services.TipoServicoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(TipoServicoController.class)
-@AutoConfigureMockMvc
+//@AutoConfigureMockMvc
+//@WebMvcTest
+//@RunWith(MockitoJUnitRunner.class)
+//@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 class TipoServicoControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Mock
+    private ServicoRepository repository;
 
-    @MockBean
-    private TipoServicoService tipoService;
+    @Mock
+    private TipoServicoRepository tipoServicoRepository;
 
-    @MockBean
-    private TipoServicoController tipoServiceController;
+    @Mock
+    private TipoServicoService tipoServicoService;
 
-    @MockBean
-    private JwtTokenUtil jwtTokenUtil;
+    @InjectMocks
+    private TipoServicoController TestClassController;
 
-    @MockBean
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @DisplayName("Trazer todos os Tipos Serviços")
+    @Test
+    void shouldReturnSuccess_FindAllTipoServices() {
 
+        TipoServico tipoServico1 = new TipoServico(1,"Manutenção", "manutencao em computadores");
+        tipoServicoRepository.save(tipoServico1);
 
+        ResponseEntity<List<TipoServicoDTO>> listTipoServicos = TestClassController.listar();
+
+        //then
+        assertThat(listTipoServicos).isNotNull();
+        assertThat(listTipoServicos.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @DisplayName("Registrar um novo Tipo Serviço")
+    @Test
+    void shouldCreateTipoServicos_ReturnSucess() {
+
+        // cenario
+        TipoServico tipoServicoteste = new TipoServico(1,"Manutenção", "manutencao em computadores");
+
+        ResponseEntity<TipoServico> result = TestClassController.cadastrar(tipoServicoteste);
+
+        ResponseEntity<List<TipoServicoDTO>> listTipoServicos = TestClassController.listar();
+
+        //then
+        assertThat(listTipoServicos).isNotNull();
+
+    }
+
+    @DisplayName("Deletar um Tipo Serviço")
+    @Test
+    void shouldDestroyTipoServico() {
+
+        TipoServico tipoServicoteste = new TipoServico(1,"Manutenção", "manutencao em computadores");
+
+        ResponseEntity<TipoServico> cad = TestClassController.cadastrar(tipoServicoteste);
+        tipoServicoRepository.save(tipoServicoteste);
+        String result = TestClassController.deletar(1);
+
+        assertThat(cad.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    }
+
+    @DisplayName("Atualizar um Tipo Serviço")
+    @Test
+    void shouldUpdateTipoServico() {
+        // cenario
+        TipoServico tipoServicoteste = new TipoServico(1,"Manutenção", "manutencao em computadores");
+        TipoServico novoTipoServico = new TipoServico("Aulas de Musica", "Aulas de Musica");
+        tipoServicoRepository.save(tipoServicoteste);
+
+        when(tipoServicoRepository.findById(any())).thenReturn(Optional.of(tipoServicoteste));
+
+        Assertions.assertDoesNotThrow(() -> TestClassController.atualizar(novoTipoServico, 1));
+    }
+
+    @DisplayName("Busca Tipo Serviço por id")
+    @Test
+    void shouldaFindTipoServicoForId() {
+        // cenario
+        TipoServico tipoServicoteste = new TipoServico(1,"Manutenção", "manutencao em computadores");
+        tipoServicoRepository.save(tipoServicoteste);
+
+        when(tipoServicoRepository.findById(any())).thenReturn(Optional.of(tipoServicoteste));
+
+        Assertions.assertDoesNotThrow(() -> TestClassController.servicosByTipoServicos(1));
+    }
 }
