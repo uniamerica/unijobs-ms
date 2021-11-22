e8abfd6e7b9f0d867c15a0fd67492466e5681ea0
DucilioManjate <liomanjate@gmail.com>
2021-11-22 18:49:37 -0300

    endpoint by entity profile

---
+++ src/main/java/com/uniamerica/unijobsbackend/controllers/PerfilController.java
@@ -0,0 +1,56 @@
+package com.uniamerica.unijobsbackend.controllers;
+
+
+import com.uniamerica.unijobsbackend.models.Perfil;
+import com.uniamerica.unijobsbackend.services.PerfilService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/profiles")
+public class PerfilController {
+
+    private PerfilService perfilService;
+
+    @Autowired
+
+    public PerfilController(PerfilService perfilService){
+        this.perfilService = perfilService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Perfil> store(@Valid @RequestBody Perfil perfil) {
+        return ResponseEntity.ok(perfilService.store(perfil));
+    }
+    @GetMapping
+    public ResponseEntity<List<Perfil>> index() {
+        return ResponseEntity.ok(perfilService.index());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Perfil>> show(@PathVariable Integer id) {
+        return ResponseEntity.ok(perfilService.show(id));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Perfil> update(@PathVariable Integer id, @Valid @RequestBody Perfil perfil) {
+        Optional<Perfil> optionalPerfil = perfilService.show(id);
+
+        if (optionalPerfil.isPresent()) {
+            perfil.setId(id);
+            return ResponseEntity.ok(perfilService.update(perfil));
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Perfil> destroy(@PathVariable Integer id) {
+        perfilService.destroy(id);
+        return ResponseEntity.ok(null);
+    }
+}
---
+++ src/main/java/com/uniamerica/unijobsbackend/repositories/PerfilRepository.java
@@ -0,0 +1,7 @@
+package com.uniamerica.unijobsbackend.repositories;
+
+import com.uniamerica.unijobsbackend.models.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfilRepository extends JpaRepository<Perfil, Integer> {
+}
