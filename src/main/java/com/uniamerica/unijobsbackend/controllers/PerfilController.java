3be6e52648c5662351b63d7fc53bf1951751cfe1
DucilioManjate <liomanjate@gmail.com>
2021-11-26 17:10:45 -0300

    UserService

--- src/main/java/com/uniamerica/unijobsbackend/controllers/PerfilController.java
+++ src/main/java/com/uniamerica/unijobsbackend/controllers/PerfilController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/profiles")
+@RequestMapping("/profiles")
 public class PerfilController {
 
     private PerfilService perfilService;
---
+++ src/main/java/com/uniamerica/unijobsbackend/services/UsuarioService.java
@@ -0,0 +1,44 @@
+package com.uniamerica.unijobsbackend.services;
+
+
+import com.uniamerica.unijobsbackend.models.Usuario;
+import com.uniamerica.unijobsbackend.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario store(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public List<Usuario> index() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> show(Integer id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario update(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void destroy(Integer id) {
+        Optional<Usuario> optionalUsuario = this.show(id);
+
+        if (optionalUsuario.isPresent()) {
+            usuarioRepository.deleteById(id);
+        }
+    }
+}
