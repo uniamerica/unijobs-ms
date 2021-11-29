1e534962531fb421f8a28be18ef7661f3c130453
JonasXPX <jonasdefarias@hotmail.com>
2021-11-29 12:55:40 -0300

    feat/included product search

--- src/main/java/com/uniamerica/unijobsbackend/repositories/RepositorioProduto.java
+++ src/main/java/com/uniamerica/unijobsbackend/repositories/RepositorioProduto.java
@@ -2,7 +2,10 @@ package com.uniamerica.unijobsbackend.repositories;
 
 import com.uniamerica.unijobsbackend.models.Produto;
 import com.uniamerica.unijobsbackend.models.TipoProduto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,7 @@ public interface RepositorioProduto extends JpaRepository<Produto, Integer> {
     List<Produto> findTop5ByOrderByUpdatedAtDesc();
 
     List<Produto> findByTipoProduto(TipoProduto tipoProduto);
+
+    @Query("select p from Produto p where upper(p.titulo) like upper(?1)")
+    Page<Produto> findBySearch(String search, Pageable pageable);
 }
